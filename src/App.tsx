9082693380a381d15e8fc80ccc65e953afcf311f
legionaire13 <legionaire13@mail.ru>
2022-01-30 18:42:25 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Form, Input, Button, Checkbox } from "antd"
+
+import "antd/dist/antd.css"
+import "./App.css"
 
 function App() {
+  const onFinish = (values: any) => {
+    console.log("Success:", values)
+  }
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off">
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            { message: "Please input your username!" },
+            {
+              min: 8,
+              message: "Username is less than 8 characters!",
+            },
+            {
+              max: 14,
+              message: "Username is more than 10 characters!",
+            },
+          ]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}>
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
